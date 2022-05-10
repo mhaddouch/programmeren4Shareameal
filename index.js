6152aeecffe52c308db98bc796ac00e00163be17
Mohamed <m.haddouch1@student.avans.nl>
2022-05-10 17:09:35 +0200

    opniew beginnen met vorige code

--- index.js
+++ index.js
@@ -2,33 +2,19 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
+const result = {
+code : 200,
+message :'Hello world',
+};
 
-let database = [];
-let id = 0;
+const server = http.createServer((req,res)=>{
+  res.statusCode = 200;
+  res.setHeader('Content-Type','text/plain');
+  res.end(JSON.stringify(result));
 
-app.all("*", (req, res, next) => {
-  const method = req.method;
-  console.log(`Method ${method} is aangeroepen`);
-  next();
 });
 
-
-app.get("/", (req, res) => {
-  res.status(200).json({
-    status: 200,
-    result: "Hello World",
-  });
+server.listen(port, () => {
+  console.log(`Serving running at http://${port}/`);
 });
 
-app.all("*", (req, res) => {
-  res.status(401).json({
-    status: 401,
-    result: "End-point not found",
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
