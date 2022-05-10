067c9d6960b3cc7879da03b604aa292a56ff045f
Mohamed <m.haddouch1@student.avans.nl>
2022-05-10 15:42:46 +0200

    opniew beginnen

--- index.js
+++ index.js
@@ -1,73 +1,21 @@
-const express = require("express");
-const app = express();
+const text = require('body-parser/lib/types/text');
+const { contentType } = require('express/lib/response');
+const res = require('express/lib/response');
+const http = require('http');
+
 const port = process.env.PORT || 3000;
+const result = {
+code : 200,
+message :'Hello world',
+};
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
+const server = http.createServer((req,res)=>{
+  res.statusCode = 200;
+  res.setHeader('Content-Type','text/plain');
+  res.end(JSON.stringify(result));
 
-let database = [];
-let id = 0;
-
-app.all("*", (req, res, next) => {
-  const method = req.method;
-  console.log(`Method ${method} is aangeroepen`);
-  next();
 });
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    status: 200,
-    result: "Hello World",
-  });
-});
-
-app.post("/api/movie", (req, res) => {
-  let movie = req.body;
-  id++;
-  movie = {
-    id,
-    ...movie,
-  };
-  console.log(movie);
-  database.push(movie);
-  res.status(201).json({
-    status: 201,
-    result: database,
-  });
-});
-
-app.get("/api/movie/:movieId", (req, res, next) => {
-  const movieId = req.params.movieId;
-  console.log(`Movie met ID ${movieId} gezocht`);
-  let movie = database.filter((item) => item.id == movieId);
-  if (movie.length > 0) {
-    console.log(movie);
-    res.status(200).json({
-      status: 200,
-      result: movie,
-    });
-  } else {
-    res.status(401).json({
-      status: 401,
-      result: `Movie with ID ${movieId} not found`,
-    });
-  }
-});
-
-app.get("/api/movie", (req, res, next) => {
-  res.status(200).json({
-    status: 200,
-    result: database,
-  });
-});
-
-app.all("*", (req, res) => {
-  res.status(401).json({
-    status: 401,
-    result: "End-point not found",
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+server.listen(port, () => {
+  console.log(`Serving running at http://${port}/`);
 });
