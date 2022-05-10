edcfcceda2e4350c8052701cc6b5cb72f40c25b8
Mohamed <m.haddouch1@student.avans.nl>
2022-05-10 17:02:47 +0200

    opniew beginnen

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-
-const http = require("http");
-// const hostname = "127.0.0.1";
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("Hello World\n");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+let database = [];
+let id = 0;
+
+app.all("*", (req, res, next) => {
+  const method = req.method;
+  console.log(`Method ${method} is aangeroepen`);
+  next();
 });
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    result: "Hello World",
+  });
+});
 
-server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.all("*", (req, res) => {
+  res.status(401).json({
+    status: 401,
+    result: "End-point not found",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
