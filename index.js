92d159ec3681830369d37a25447007c9722cf2e6
Mohamed <m.haddouch1@student.avans.nl>
2022-05-12 14:57:14 +0200

    opniew beginnen met falende code kijken of het werkt

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-const express = require("express");
-const app = express();
+const http = require("http");
+// const hostname = "127.0.0.1";
 const port = process.env.PORT || 3000;
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-let database = [];
-let id = 0;
-app.all("*", (req, res, next) => {
-  const method = req.method;
-  console.log(`Method ${method} is aangeroepen`);
-  next();
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("Hello World\n");
 });
 
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    status: 200,
-    result: "Hello World",
-  });
-});
-app.all("*", (req, res) => {
-  res.status(401).json({
-    status: 401,
-    result: "End-point not found",
-  });
-});
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+
+
+server.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
