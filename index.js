faa4724b9ad9b68c83a9e6fe5cfe2c6a18d668d3
Mohamed <m.haddouch1@student.avans.nl>
2022-05-12 16:21:34 +0200

    check depoyemnt

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 
-const http = require("http");
-// const hostname = "127.0.0.1";
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("Hello World\n");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-
-
-server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
 
