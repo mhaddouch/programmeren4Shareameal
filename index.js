58df8242512f5bb8ec360818590a69350d6c281b
Mohamed <m.haddouch1@student.avans.nl>
2022-05-23 13:18:20 +0200

    opniew beginnen met werkende code

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 
 const express = require('express');
+const res = require('express/lib/response');
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+let database = [];
+let id =0;
+
+app.all('*', (req, res, next) => {
+  const method = req.method
+  console.log(`Method ${method} is aangeroepen`);
+  next();
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.status(200).json({
+    status:200,
+    result:'hallo world'
+  })
+});
+
+app.post('/api/movie',(req,res) =>{
+  let movie = req.body;
+  console.log(movie);
+  id++;
+
+  movie={
+    id,
+    ...movie,
+  };
+
+  database.push(movie);
+  console.log(database);
+  res.status(201).json({
+    status:201,
+    result: movie,
+  });
+});
+
+app.all('*',(req,res)=>{
+  res.status(404).json({
+    status:404,
+    result:'End-point not found',
+  });
 });
 
 app.listen(port, () => {
