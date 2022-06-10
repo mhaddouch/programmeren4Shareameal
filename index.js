3a486ffc3d5ac1be9c26a4c15f78d019ffee1779
Mohamed <m.haddouch1@student.avans.nl>
2022-06-10 14:37:19 +0200

    database toegevoegd

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+require('dotenv').config()
+
+
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
