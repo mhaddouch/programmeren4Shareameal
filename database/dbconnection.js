6feabdb9b189bb9660ebd6d778a073cb022f6d1c
mhaddouc1 <m.haddouch1@student.avans.nl>
2022-07-01 14:58:53 +0200

    deploy werkend

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -1,18 +1,19 @@
-const mysql = require('mysql')
+const mysql = require('mysql2')
 const logger = require('../config/config').logger
 require('dotenv').config()
 
+
 const dbConfig = {
-    connectionLimit: 10,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    multipleStatements: true,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+  connectionLimit: 10,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  multipleStatements: true,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 }
 
 logger.debug(dbConfig)
@@ -20,15 +21,15 @@ logger.debug(dbConfig)
 const pool = mysql.createPool(dbConfig)
 
 pool.on('connection', function (connection) {
-    logger.debug(`Connected to database '${connection.config.database}'`)
+  logger.debug(`Connected to database '${connection.config.database}'`)
 })
 
 pool.on('acquire', function (connection) {
-    logger.debug('Connection %d acquired', connection.threadId)
+  logger.debug('Connection %d acquired', connection.threadId)
 })
 
 pool.on('release', function (connection) {
-    logger.debug('Connection %d released', connection.threadId)
+  logger.debug('Connection %d released', connection.threadId)
 })
 
 module.exports = pool
