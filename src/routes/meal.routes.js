2c675d4c62671e3c6902d6eb66c9f44915f11588
mhaddouc1 <m.haddouch1@student.avans.nl>
2022-07-01 12:10:20 +0200

    add meal

---
+++ src/routes/meal.routes.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const { getUserProfile } = require('../controllers/meal.controller');
+const router = express.Router();
+const userController = require('../controllers/meal.controller')
