17db5dacf91d9c2a87b5b7bf095c70188d111cce
mhaddouc1 <m.haddouch1@student.avans.nl>
2022-07-03 13:43:35 +0200

    testing werkt

--- src/routes/meal.routes.js
+++ src/routes/meal.routes.js
@@ -2,31 +2,3 @@ const express = require('express');
 const { getUserProfile } = require('../controllers/meal.controller');
 const router = express.Router();
 const mealController = require('../controllers/meal.controller')
-
-
-router.get("/", (req, res) => {
-    res.status(200).json({
-      status: 200,
-      result: "Share a meal application",
-    });
-  });
-  
-  router.post("/api/meal",mealController.validateMeal,mealController.addMeal);
-
-  
-  //router.get("/api/meal", userController.getAllUsers);
-  
-  router.get("/api/meal/profile", (req, res) => {
-    res.status(401).json({
-      status: 401,
-      result: "This feature has not been implemented yet.",
-    });
-  });
-  
-  router.get("/api/meal/:mealId", mealController.getMealId);
-  
-  router.put("/api/meal/:id", mealController.updateMeal);
-  
-  router.delete("/api/meal/:mealId", mealController.deleteMeal);
-
-  module.exports = router;
